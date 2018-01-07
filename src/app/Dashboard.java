b3771fa9cca7a6cd419403633fa9b1ccaa9fdcdb
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-01-07 14:50:48 +0700

    [Update] Dashboard
    - Enum to check for session stats
    - Recursive call to restart apps

--- src/app/Dashboard.java
+++ src/app/Dashboard.java
@@ -1,5 +1,6 @@
 package app;
 
+import app.utils.Status;
 import app.views.login.LoginPresenter;
 import app.views.login.LoginView;
 import app.views.menu.MenuPresenter;
@@ -7,12 +8,58 @@ import app.views.menu.MenuView;
 
 public class Dashboard {
 
+    private static Status status = Status.AUTH;
+
     public static void main(String[] args) {
+
+        switch (status) {
+            case STOP:
+
+                // do nothing or exit?
+                System.exit(status.getAbbreviation());
+                break;
+            case MENU:
+
+                initMenu();
+                break;
+
+            case DATA:
+
+                initPatient();
+                initDoctor();
+                initTransaction();
+                break;
+
+            default:
+                initDefault();
+        }
+
+        main(args); // restart apps
+    }
+
+    private static void initDefault() {
         initLogin();
-        initMenu();
+        initRegist();
+
+        status = Status.MENU;
+    }
+
+    private static void initTransaction() {
+
+    }
+
+    private static void initDoctor() {
+
+    }
+
+    private static void initPatient() {
+
     }
 
     private static void initMenu() {
+
+        status = Status.MENU;
+
         MenuPresenter menuPresenter = new MenuPresenter();
         MenuView menuView = new MenuView();
 
@@ -22,7 +69,15 @@ public class Dashboard {
         menuView.showView();
     }
 
+    private static void initRegist() {
+
+        status = Status.AUTH;
+    }
+
     private static void initLogin() {
+
+        status = Status.AUTH;
+
         LoginPresenter loginPresenter = new LoginPresenter();
         LoginView loginView = new LoginView();
 
