9997df3e6f91928c9e95b18d86babccc3a36c13b
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-01-12 09:55:59 +0700

    [MERGED] Patient & Persist

---
+++ src/app/libs/net/sf/persist/RuntimeSQLException.java
@@ -0,0 +1,20 @@
+// $Id$
+
+package app.libs.net.sf.persist;
+
+@SuppressWarnings("serial")
+public final class RuntimeSQLException extends RuntimeException {
+
+	public RuntimeSQLException(final Throwable cause) {
+		super(cause);
+	}
+
+	public RuntimeSQLException(final String message) {
+		super(message);
+	}
+
+	public RuntimeSQLException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+}
