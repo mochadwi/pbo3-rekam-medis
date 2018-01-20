81d001d086e5fd732d2159b3c91699017a670364
Mochamad Iqbal Dwi Cahyo <mochadwi@users.noreply.github.com>
2018-01-21 05:46:20 +0700

    F/medicines (#29)
    * [FIX] Login and add new library
    
    * [FIX] Log4J & Connection to DB
    
    * [FIX] Doctor CRUD
    
    * [FIX] Medical Record CRUD
    
    * [FIX] Medicine CRUD
    
    * [FIX] User CRUD
    
    * [FIX] CRUD
    
    * [FIX] CRUD

--- src/app/views/medicine/MedicinePresenter.java
+++ src/app/views/medicine/MedicinePresenter.java
@@ -26,27 +26,43 @@ public class MedicinePresenter implements MedicineContract.Presenter {
         switch (pilihan) { 
             case "0": 
                 // do nothing 
-                view.showMenuSuccessView(); 
+                view.showMenuSuccessView("\n\n\nSucces!!\n"); 
                 Dashboard.status = Status.MENU; 
                 break; 
             case "1": 
-                view.showMenuSuccessView(); 
+                view.showMenuSuccessView("\n\n\nSucces!!\n"); 
                 view.showCreateDataView(); 
                 break; 
             case "2": 
-                view.showMenuSuccessView(); 
+                view.showMenuSuccessView("\n\n\nSucces!!\n"); 
                 view.showUpdateDataView(); 
                 break; 
             case "3": 
-                view.showMenuSuccessView(); 
+                view.showMenuSuccessView("\n\n\nSucces!!\n"); 
                 view.showDeleteDataView(); 
                 break; 
             case "4": 
-                view.showMenuSuccessView(); 
+                view.showMenuSuccessView("\n\n\nSucces!!\n"); 
                 view.showReadDataView(); 
                 break; 
+                
+            case "-1":
+                view.showMenuSuccessView("\n\n\nMedicine Data found!!\n");
+                Dashboard.status = Status.MEDICINE;
+                break;
+
+            case "-2":
+                view.showMenuSuccessView("\n\n\nData not found!!\n");
+                view.showUpdateDataView();
+                break;
+                
+            case "-3": 
+                view.showMenuSuccessView("\n\n\nData not found!!\n"); 
+                view.showDeleteDataView(); 
+                break; 
+                
             default: 
-                view.showMenuErrorView(); 
+                view.showMenuErrorView("Pilih Angka 0-4!"); 
                 view.showView(); 
         } 
  
@@ -65,14 +81,50 @@ public class MedicinePresenter implements MedicineContract.Presenter {
  
     @Override 
     public void update(DataObat dataObat) { 
- 
+        Db.getInstance().initPersist().update(dataObat);
     } 
  
     @Override 
-    public void delete(DataObat dataObat) { 
- 
+    public void delete(String kdObat) { 
+        Db.getInstance().initPersist()
+                .executeUpdate("UPDATE data_obat SET value_obat=0 WHERE kode_obat=?", kdObat);
     } 
  
+    @Override
+    public String find(String kdObat) {
+
+        DataObat dataObat = Db.getInstance().initPersist().readByPrimaryKey(DataObat.class, kdObat);
+
+        if (dataObat != null) return dataObat.getKd();
+
+        return "";
+    }
+
+    @Override
+    public String findMin() {
+        return "";
+    }
+
+    @Override
+    public String findMax() {
+
+        return Db.getInstance().initPersist().read(String.class,
+                "SELECT MAX(kode_obat) FROM data_obat");
+    }
+
+    @Override
+    public boolean validateData(String data) {
+
+        if (data.isEmpty()) {
+
+            view.showMenuErrorView("Data Tidak ditemukan!");
+            return false;
+        }
+
+
+        return true;
+    }
+    
     @Override 
     public void bind(MedicineContract.View view) { 
         this.view = view; 
